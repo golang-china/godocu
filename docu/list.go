87551d25c36e2c206d17775147ebf8aa755c2e63
YU HengChun <achun.shx@qq.com>
2016-08-12 13:27:03 +0800

    Info, json omitempty

--- docu/list.go
+++ docu/list.go
@@ -9,19 +9,21 @@ import (
 type Info struct {
 	Import   string // 导入路径
 	Synopsis string // 一句话包摘要
-	Prefix   string // 额外前缀 "main,test"
-	Ext      string // 额外扩展名 "text,md"
-	Readme   string // 该包下 readme 文件名
-	Progress int    // 翻译完成度
+	// Prefix 额外前缀 "main,test"
+	// Ext 额外扩展名 "text,md"
+	// Readme 该包下 readme 文件名
+	Prefix, Ext, Readme string `json:"omitempty"`
+	Progress            int    // 翻译完成度
 }
 
 // List 表示在同一个 repo 下全部包文档信息.
 type List struct {
-	Repo        string // 托管 git 仓库地址.
-	Description string // 一句话介绍 Repo 或列表
-	Readme      string // 整个 list 的 readme 文件名
-	Lang        string // 同一个列表具有相同的 Lang
-	Package     []Info // 所有包的信息
+	Repo string // 托管 git 仓库地址.
+	// Description 一句话介绍 Repo 或列表
+	// Readme 整个 list 的 readme 文件名
+	Description, Readme string `json:"omitempty"`
+	Lang                string // 同一个列表具有相同的 Lang
+	Package             []Info // 所有包的信息
 }
 
 // TranslationProgress 返回 file 的翻译完成度.
